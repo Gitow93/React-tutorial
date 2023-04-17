1913c1af209e9d0abbd0fa594c92bdef4a0fdc0a
Jorge <jorge.jimenezmunoz.93@gmail.com>
2023-04-17 18:03:24 +0200

    add filter for schedule

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
@@ -32,14 +32,23 @@ const Course = ({ course }) => (
 );
 
 const Banner = ({ title }) => <h1>{title}</h1>;
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    {Object.values(courses).map((course) => (
-      <Course key={course.id} course={course} />
-    ))}
-    ;
-  </div>
-);
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState("Fall");
+  const termCourses = Object.values(courses).filter(
+    (course) => term === getCourseTerm(course)
+  );
+
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+        {termCourses.map((course) => (
+          <Course key={course.id} course={course} />
+        ))}
+      </div>
+    </>
+  );
+};
 const Main = () => {
   const {
     data: schedule,
@@ -59,6 +68,38 @@ const Main = () => {
     </div>
   );
 };
+
+// Añadimos el filtro que va dentro de nuestra constante lista de cursos:
+
+const TermSelector = ({ term, setTerm }) => (
+  <div className="btn-group">
+    {Object.values(terms).map((value) => (
+      <TermButton
+        key={value}
+        term={value}
+        setTerm={setTerm}
+        checked={value === term}
+      />
+    ))}
+  </div>
+);
+
+//Creamos un botón
+const TermButton = ({ term, setTerm, checked }) => (
+  <>
+    <input
+      type="radio"
+      id={term}
+      className="btn-check"
+      checked={checked}
+      autoComplete="off"
+      onChange={() => setTerm(term)}
+    />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+      {term}
+    </label>
+  </>
+);
 
 const queryClient = new QueryClient();
 
