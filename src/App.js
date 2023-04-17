a6992ca6c8335fbcbe6ebe73d5cf8148c1815e14
Jorge <jorge.jimenezmunoz.93@gmail.com>
2023-04-17 17:55:34 +0200

    add the json with the schedule

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React from "react";
 import "./App.css";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: {
-    F101: {
-      id: "F101",
-      meets: "MWF 11:00-11:50",
-      title: "Computer Science: Concepts, Philosophy, and Connections",
-    },
-    F110: {
-      id: "F110",
-      meets: "MWF 10:00-10:50",
-      title: "Intro Programming for non-majors",
-    },
-    S313: {
-      id: "S313",
-      meets: "TuTh 15:30-16:50",
-      title: "Tangible Interaction Design and Learning",
-    },
-    S314: {
-      id: "S314",
-      meets: "TuTh 9:30-10:50",
-      title: "Tech & Human Interaction",
-    },
-  },
+const fetchSchedule = async () => {
+  const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return await response.json();
 };
 
-const Banner = ({ title }) => <h1>{title}</h1>;
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    {Object.values(courses).map((course) => (
-      <Course key={course.id} course={course} />
-    ))}
-  </div>
-);
 const terms = { F: "Fall", W: "Winter", S: "Spring" };
 
-const getCourseTerm = (course) => terms[course.id.charAt(0)];
+const getCourseTerm = (course) => {
+  const id = course.id || "";
+  return terms[id.charAt(0)] || "";
+};
 
-const getCourseNumber = (course) => course.id.slice(1, 4);
+const getCourseNumber = (course) => (course.id ? course.id.slice(1, 4) : "");
 
 const Course = ({ course }) => (
   <div className="card m-1 p-2">
@@ -54,11 +31,41 @@ const Course = ({ course }) => (
   </div>
 );
 
-const App = () => (
-  <div className="container">
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
+const Banner = ({ title }) => <h1>{title}</h1>;
+const CourseList = ({ courses }) => (
+  <div className="course-list">
+    {Object.values(courses).map((course) => (
+      <Course key={course.id} course={course} />
+    ))}
+    ;
   </div>
+);
+const Main = () => {
+  const {
+    data: schedule,
+    isLoading,
+    error,
+  } = useQuery("schedule", fetchSchedule);
+
+  if (error) return <h1>{error}</h1>;
+  if (isLoading) return <h1>Loading the schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={schedule?.title} />
+      <CourseList
+        courses={schedule?.courses ? Object.values(schedule.courses) : []}
+      />
+    </div>
+  );
+};
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Main />
+  </QueryClientProvider>
 );
 
 export default App;
